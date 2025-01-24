09645ddbb7d38aa6a86c25352d37c96822c276a0
Thales Pinto <thales.moreira1998.2@gmail.com>
2025-01-24 20:31:06 -0300

    [feat] inicializando o bd junto com o app

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,29 @@
 import 'package:flutter/material.dart';
 import 'package:reservas_app/login.dart';
+import 'package:reservas_app/services/db_service.dart'; // Substitua pelo caminho correto
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding
+      .ensureInitialized(); // Garante que os serviços Flutter estejam prontos
+  await _initializeDatabase(); // Inicializa o banco de dados
   runApp(
     MaterialApp(
       title: "Reservas App",
       debugShowCheckedModeBanner: false,
       initialRoute: Login.route,
       routes: {
-        Login.route: (context) => Login(),
-        
-      }
-    )
+        Login.route: (context) => const Login(),
+      },
+    ),
   );
 }
 
+Future<void> _initializeDatabase() async {
+  final databaseService = DatabaseService();
+  try {
+    await databaseService.initDatabase(); // Inicializa o banco de dados
+    print("Banco de dados inicializado com sucesso.");
+  } catch (e) {
+    print("Erro ao inicializar o banco de dados: $e");
+  }
+}

e947958dfeade60a24cd425e5ec106e27a4b08d4
Thales Pinto <thales.moreira1998.2@gmail.com>
2025-01-25 18:00:19 -0300

    [feat] função que cria reservas

--- lib/services/booking_service.dart
+++ lib/services/booking_service.dart
@@ -4,6 +4,19 @@ import 'package:reservas_app/services/db_service.dart';
 class BookingService {
   final DatabaseService _databaseService = DatabaseService();
 
+  /// Insere um novo registro de booking no banco de dados.
+  Future<int> insertBooking(Booking booking) async {
+    final db = await _databaseService.getDatabaseInstance();
+
+    final int id = await db.insert(
+      'booking',
+      booking.toMap(), // Dados do booking convertidos para Map
+    );
+
+    print('Booking inserido com sucesso com ID $id.');
+    return id;
+  }
+
   /// Filtra os bookings associados a um determinado userId.
   Future<List<Booking>> getBookingsByUserId(int userId) async {
     final db = await _databaseService.getDatabaseInstance();

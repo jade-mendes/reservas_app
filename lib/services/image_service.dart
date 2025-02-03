6fcdb7178204c4fb069d1e8dc2ab035514897b42
Thales Pinto <thales.moreira1998.2@gmail.com>
2025-02-03 14:40:38 -0300

    update: mudando nome Image pq da conflito com class builtin do dart

--- lib/services/image_service.dart
+++ lib/services/image_service.dart
@@ -5,11 +5,11 @@ class ImageService {
   final DatabaseService _databaseService = DatabaseService();
 
   // Função para inserir uma imagem na tabela 'images'
-  Future<int> insertImage(Image image) async {
+  Future<int> insertImage(ImageCustom image) async {
     final db = await _databaseService.getDatabaseInstance();
     final int id = await db.insert(
       'images',
-      image.toMap(), // Mapeia o objeto Image para um Map
+      image.toMap(), // Mapeia o objeto ImageCustom para um Map
     );
 
     print('Imagem inserida com sucesso com ID $id.');

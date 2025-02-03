98b7e5cc810e8b92ae017577ca7853ec0322c5f0
Thales Pinto <thales.moreira1998.2@gmail.com>
2025-02-03 14:37:02 -0300

    feat: alterações necessárias nos models de address e image

--- lib/models/image.dart
+++ lib/models/image.dart
@@ -1,24 +1,24 @@
-class Image {
+class ImageCustom {
   final int? id; // Pode ser nulo para novos registros
   final int propertyId;
   final String path;
 
-  Image({
+  ImageCustom({
     this.id,
     required this.propertyId,
     required this.path,
   });
 
-  // Converte um Map (como os resultados de uma consulta ao banco) para um objeto Image
-  factory Image.fromMap(Map<String, dynamic> map) {
-    return Image(
+  // Converte um Map (como os resultados de uma consulta ao banco) para um objeto ImageCustom
+  factory ImageCustom.fromMap(Map<String, dynamic> map) {
+    return ImageCustom(
       id: map['id'] as int?,
       propertyId: map['property_id'] as int,
       path: map['path'] as String,
     );
   }
 
-  // Converte o objeto Image para um Map para ser salvo no banco
+  // Converte o objeto ImageCustom para um Map para ser salvo no banco
   Map<String, dynamic> toMap() {
     return {
       'id': id,
